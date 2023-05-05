7bd47f9217b221721c33a825b6d29d6766f1126d
ooothinhooo <74719505+ooothinhooo@users.noreply.github.com>
2023-05-06 00:01:39 +0700

    UPDATE

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -24,5 +24,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const adminUser = ['admin@gmail.com']
+export const adminUser = ["admin@gmail.com", "vanthinh@gmail.com"];
 export default app;
